696c079b4cee4097ddda66329ac05fcd0f751ab2
zhshize <joe7086543@hotmail.com>
2019-07-12 00:17:45 +0800

    Add docs

--- src/bridgeRpc.ts
+++ src/bridgeRpc.ts
@@ -7,10 +7,27 @@ import msgpack from 'msgpack-lite'
 import RpcError from './rpcError'
 import OperationTimeoutError from './operationTimeoutError'
 
+/**
+ * RPC Request handler must process a request and return a [[RpcResponse]].
+ */
 export type RpcHandler = (request: RpcRequest) => RpcResponse
+
+/**
+ * RPC Notification handler must process a request, nothing should be returned.
+ */
 export type RpcNotificationHandler = (request: RpcRequest) => void
 
+/**
+ * [[BridgeRpc]] is an Bridge RPC client can connect to Bridge RPC server and
+ * register methods and emit a request or a notification.
+ */
 export default class BridgeRpc {
+  /**
+   * Initialize an [[BridgeRpc]] object, start receiving/sending after called
+   * [[connect]].  Suggest you register the handling methods before you call
+   * [[connect]].
+   * @param url Url of the server to be connected.
+   */
   public constructor(url: string) {
     this.url = url
     this.handlers = new RequestHandlerDictionary()
@@ -24,6 +41,10 @@ export default class BridgeRpc {
   protected notificationHandlers: NotificationHandlerDictionary
   protected callbacks: CallbackDictionary
 
+  /**
+   * Get [[WebSocket]] object maintained by [[BridgeRpc]] object.
+   * **BE CAREFUL** to access this getter.
+   */
   get rawSocket(): WebSocket {
     if (this._rawSocket === null) {
       this.connect()
@@ -31,20 +52,38 @@ export default class BridgeRpc {
     return this._rawSocket as WebSocket
   }
 
+  /**
+   * Connect to the server.
+   * Suggest you register the handling methods before you call [[connect]].
+   */
   public connect() {
     this._rawSocket = new WebSocket(this.url)
     this._rawSocket.binaryType = 'arraybuffer'
     this._rawSocket.onmessage = this.onMessage.bind(this)
   }
 
+  /**
+   * An event listener to be called when the server is connected.
+   * @param connected The event listener.
+   */
   public onConnect(connected: () => void): void {
     this.rawSocket.onopen = connected
   }
 
+  /**
+   * An event listener with method name to be called when received a request.
+   * @param method The name of the method will be registered.
+   * @param handler The event listener, must returned a [[RpcResponse]].
+   */
   public onRequest(method: string, handler: RpcHandler): void {
     this.handlers[method] = handler
   }
 
+  /**
+   * An event listener with method name to be called when received a notification.
+   * @param method The name of the method will be registered.
+   * @param handler The event listener, no return value.
+   */
   public onNotify(method: string, handler: RpcNotificationHandler): void {
     const m = this.notificationHandlers[method]
     if (m === null || m === undefined) {
@@ -53,6 +92,12 @@ export default class BridgeRpc {
     this.notificationHandlers[method].push(handler)
   }
 
+  /**
+   * Call remote method.
+   * @param method The method name to be called.
+   * @param data Parameters to be sent.
+   * @param timeoutMilliSeconds Timeout of request operation, unit is millisecond.
+   */
   public request(
     method: string,
     data: any,
@@ -79,6 +124,11 @@ export default class BridgeRpc {
     })
   }
 
+  /**
+   * Call remote method without response.
+   * @param method The method name to be called.
+   * @param data Parameters to be sent.
+   */
   public notify(method: string, data: any): void {
     this.rawSocket.send(
       msgpack.encode({
@@ -90,6 +140,10 @@ export default class BridgeRpc {
     )
   }
 
+  /**
+   * An event listener for [[WebSocket]] onmessage to handle BridgeRpc message.
+   * @param event Event object from WebSocket.onmessage
+   */
   protected onMessage(event: any) {
     const data = msgpack.decode(new Uint8Array(event.data))
     if (data.method !== undefined && data.method !== null) {
@@ -159,6 +213,15 @@ export default class BridgeRpc {
     }
   }
 
+  /**
+   * Close the connection.
+   * @param code A numeric value indicating the status code explaining why the connection is being
+   * closed. If this parameter is not specified, a default value of 1005 is assumed. See the [list
+   * of status codes](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent#Status_codes)
+   * of CloseEvent for permitted values.
+   * @param reason A human-readable string explaining why the connection is closing. This string
+   * must be no longer than 123 bytes of UTF-8 text (**not** characters).
+   */
   public close(code: number, reason: string): void {
     this.rawSocket.close(code, reason)
   }
